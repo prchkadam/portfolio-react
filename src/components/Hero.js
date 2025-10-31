bfd06ddf9570677d69f087184742a4a76db2e611
prchkadam <divyakadam207@gmail.com>
2025-10-31 19:51:18 +0530

    Hero page edit

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero=()=>{
+    return(
+        <>
+        <h1>Divya Kadam</h1>
+        <h2>Full Stack Developer</h2>
+        </>
+    )
+}
+
+export default Hero;
